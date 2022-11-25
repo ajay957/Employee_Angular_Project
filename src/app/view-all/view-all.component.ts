eddcd4fa41f5f4436d87e1aeaed58b8a0ffdeae9
Ajay957 <ajay74.aa82@gmail.com>
2022-11-25 11:09:53 +0530

    data binding commit

--- src/app/view-all/view-all.component.ts
+++ src/app/view-all/view-all.component.ts
@@ -6,5 +6,37 @@ import { Component } from '@angular/core';
   styleUrls: ['./view-all.component.css']
 })
 export class ViewAllComponent {
+  data:any=[
+    {
+      "name":"ajay",
+      "designation":"TMG",
+      "company":"nest",
+      "salary":25000
+    },
+    {
+      "name":"abel",
+      "designation":"TMG",
+      "company":"cintel",
+      "salary":25000
+    },
+    {
+      "name":"antony",
+      "designation":"TMG",
+      "company":"nest",
+      "salary":25000
+    },
+    {
+      "name":"alfin",
+      "designation":"TMG",
+      "company":"gadgeon",
+      "salary":25000
+    },
+    {
+      "name":"alex",
+      "designation":"TMG",
+      "company":"nest",
+      "salary":25000
+    }
+  ]
 
 }
